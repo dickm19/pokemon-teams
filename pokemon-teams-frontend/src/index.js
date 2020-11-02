566477e0710d85584aaaed48e3891cb554a8a706
dickm19 <maryclairedick@gmail.com>
2020-11-02 15:22:35 -0500

    Pseudocoded final product

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,4 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
     const BASE_URL = "http://localhost:3000"
     const TRAINERS_URL = `${BASE_URL}/trainers`
     const POKEMONS_URL = `${BASE_URL}/pokemons`
@@ -56,29 +56,40 @@
 
             //append pokemons to card via forEach
             trainer.pokemons.forEach(pokemon =>{ 
+                //create li for each pokemon and set id to trainer id
                  const li = document.createElement("li")
                  li.dataset.id = pokemon.id
+
+                 //create button element for each pokemon, set class, id, and and text content
                  const deleteButton = document.createElement("button")
                  deleteButton.classList.add("release")
                  deleteButton.textContent = "Release"
                  deleteButton.setAttribute("data-pokemon-id", pokemon.id)
+
+                 //set inner HTML for li element
                  li.innerHTML = `
                  ${pokemon.nickname} (${pokemon.species})
                  `
-                 li.append(deleteButton)
+                 
+                 //add delete event listener that calls releasePokemon function
                  deleteButton.addEventListener("click", releasePokemon)
+
+
+                 //append button to li and li to ul
+                 li.append(deleteButton)
                  ul.append(li)
             })
             
+            //append each card to main upon creation
             main.append(card)
         })
     }
     
     
 
-
+    //fetch pokemon function
     function getPokemon(event) {
-
+        //set config attributes
         const config = {
             method: "POST",
             headers: { "Content-Type": "application/json"},
@@ -86,13 +97,17 @@
                 "trainer_id": event.target.parentElement.dataset.id
             })
         }
-        
+        //perform fetch request with a limit of 1
         return fetch(`${POKEMONS_URL}/?_limit=1`, config)
         .then(response => response.json())
         .then(pokemon =>{
-            
+            //find the ul for the button that was clicked
             const ul = event.target.parentElement.querySelector("ul")
+
+            //create a new li for the pokemon being added
             const li = document.createElement("li")
+
+            //conditional to prevent more than 6 pokemon from being added to the ul
             if (ul.children.length < 6){
                 li.innerHTML = `
                  ${pokemon.nickname} (${pokemon.species})
@@ -100,6 +115,7 @@
                  `
                 ul.append(li)
             } else {
+                //add alert for if the user tries to add too many pokemons
                 alert("Cannot add more than six pokemon to team")
             }
             
@@ -107,20 +123,23 @@
 
     }
 
-    // pokemon => addPokemon(pokemon, event.target)
+    
     
 
     //function to release pokemon
     function releasePokemon(event){
+        //set config variable and attributes
         const config = {
             method: "DELETE",
             headers: { "Content-Type": "application/json"},
             
         }
         
+        //fetch request for specific pokemon to be deleted
         return fetch(`${POKEMONS_URL}/${event.target.parentElement.dataset.id}`, config)
         .then(response => response.json())
-        .then(pokemon =>{
+        .then(function(){
+            //delete pokemon for the button that was clicked
             event.target.parentElement.remove()
         })
     }
@@ -129,3 +148,4 @@
 
 
 
+})
